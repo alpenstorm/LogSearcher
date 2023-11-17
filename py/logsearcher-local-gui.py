cd853986996fc7168ffb8a223f70e43549c078f3
alpenstorm <mihaiedmund@icloud.com>
2023-11-17 12:12:01 -0500

    added smaller widgets, starting new gui

--- py/logsearcher-local-gui.py
+++ py/logsearcher-local-gui.py
@@ -13,12 +13,12 @@ root.geometry("1280x720")
 root.title("LogSearcher - Local Searcher")
 
 # import buttons
-widget_browse_def = PhotoImage(file='widgets/browse-def.png')
-widget_browse_hover = PhotoImage(file='widgets/browse-hover.png')
-widget_browse_pressed = PhotoImage(file='widgets/browse-pressed.png')
-widget_submit_def = PhotoImage(file='widgets/submit-def.png')
-widget_submit_hover = PhotoImage(file='widgets/submit-hover.png')
-widget_submit_pressed = PhotoImage(file='widgets/submit-click.png')
+widget_browse_def = PhotoImage(file='widgets/browse-def (Custom).png')
+widget_browse_hover = PhotoImage(file='widgets/browse-hover (Custom).png')
+widget_browse_pressed = PhotoImage(file='widgets/browse-pressed (Custom).png')
+widget_submit_def = PhotoImage(file='widgets/submit-def (Custom).png')
+widget_submit_hover = PhotoImage(file='widgets/submit-def (Custom).png')
+widget_submit_pressed = PhotoImage(file='widgets/submit-click (Custom).png')
 #widget_searchbar = PhotoImage(file='widgets/search.png') #commented out because searchbar doesn't work - might readd later?
 
 # button functions
@@ -55,7 +55,6 @@ def savefiles():
     print(sfilename)
     submit_filep()
 
-
 def submit_filep():
     global fn
     fn = filename
@@ -65,51 +64,10 @@ def submit_filep():
         fileHandle = open(fn, "r", encoding="utf8", errors="ignore")
     except:
         messagebox.showerror("Warning", "Could not read file!")
-        #file_path_entry.delete(0, END)
         submit_filep(fn)
-    
-    global w3
-    w3 = tk.Toplevel(root)
-    w2.withdraw()
-    w3.focus
-    w3.title("LogSearcher - Local Searcher")
-    w3.geometry("1280x720")
-
-    Label(w3, text="Enter Search Term", font=("Roboto", 24)).pack(side=tk.TOP, padx=10, pady=10)
-    global trm_entry
-    trm_entry = tk.Entry(w3, width=40, font=("Roboto", 16))
-    trm_entry.pack(side=tk.TOP, padx=10, pady=10)
-    global w3submit
-    w3submit = Button(w3, image=widget_submit_def, command=submit_filenames, borderwidth=0)
-    w3submit.pack(side=tk.TOP, padx=10, pady=10)
-    changeOnHover(w3submit, widget_submit_hover, widget_submit_def)
-
 
 def submit_searcht():
     messagebox.showinfo("Notice", "LogSearcher GUI will create a file with the search terms!")
-
-    global w2
-    w2 = tk.Toplevel(root)
-    root.withdraw()
-    w2.focus
-    w2.title("LogSearcher - Local Searcher")
-    w2.geometry("1280x720")
-
-    Label(w2, text="Save File", font=("Roboto", 24)).pack(side=tk.TOP, padx=10, pady=10)
-    
-    #filen_entry = tk.Entry(w3, width=40, font=("Roboto", 16))
-    #filen_entry.pack(side=tk.TOP, padx=10, pady=10)
-    
-    #foldern_entry = tk.Entry(w3, width=40, font=("Roboto", 16))
-    #foldern_entry.pack(side=tk.TOP, padx=10, pady=10)
-
-    global w2fp
-    w2fp = Button(w2, image=widget_browse_def, command=savefiles, borderwidth=0)
-    w2fp.pack(side=tk.TOP, padx=10, pady=10)
-    changeOnHover(w2fp, widget_browse_hover, widget_browse_def)
-
-    #Button(w3, text="Submit", font=("Roboto", 18), command=submit_filenames).pack(side=tk.TOP, padx=10, pady=10)
-
 
 def submit_filenames():
 
@@ -138,21 +96,45 @@ def submit_filenames():
         res = messagebox.askquestion('Search Again?', 'Do you want to search another file?')
 
         if res == 'yes':
-            w3.withdraw()
             root.deiconify()
         else:
             root.destroy()
             
 
-#tk items
-Label(root, text="File To Open", font=("Roboto", 24)).pack(side=tk.TOP, padx=10, pady=10)
-fp_button = Button(root, image=widget_browse_def, command=browseFiles, borderwidth=0)
-fp_button.pack(side=tk.TOP, padx=10, pady=10)
-#Button(root, text="Submit", font=("Roboto", 18), command=submit_filep).pack(side=tk.TOP, padx=10, pady=10)
-#file_path_entry = tk.Entry(root, width=40, font=("Roboto", 16))
-#file_path_entry.pack(side=tk.TOP, padx=10, pady=10)
+#tk labels
+lsl       = Label(root, text="Local Searcher", font=("Roboto,", 56))
+fto       = Label(root, text="File To Open", font=("Roboto", 24))
+sfl       = Label(root, text="File to Save", font=("Roboto", 24))
+stl       = Label(root, text="Enter Search Term", font=("Roboto", 24))
 
+#tk buttons
+fp_button = Button(root, image=widget_browse_def, command=browseFiles, borderwidth=0)
+w3submit  = Button(root, image=widget_submit_def, command=submit_filenames, borderwidth=0)
+w2fp      = Button(root, image=widget_browse_def, command=savefiles, borderwidth=0)
+
+# tk entry
+trm_entry = tk.Entry(root, width=40, font=("Roboto", 16))
+
+# top label
+lsl.pack(side=tk.TOP)
+
+#group 1
+fto.pack(side=tk.LEFT, padx=10, pady=10, anchor=CENTER)
+fp_button.pack(side=tk.LEFT, padx=10, pady=10, anchor=CENTER)
+
+#group 2
+sfl.pack(side=tk.LEFT, padx=10, pady=10, anchor=S)
+w2fp.pack(side=tk.LEFT, padx=10, pady=10, anchor=S)
+
+#group 3
+stl.pack(side=tk.BOTTOM, padx=10, pady=10, anchor=S)
+trm_entry.pack(side=tk.BOTTOM, padx=10, pady=10, anchor=S)
+w3submit.pack(side=tk.BOTTOM, padx=10, pady=10, anchor=S)
+
+#hovers
 changeOnHover(fp_button, widget_browse_hover, widget_browse_def)
+changeOnHover(w2fp, widget_browse_hover, widget_browse_def)
+changeOnHover(w3submit, widget_submit_hover, widget_submit_def)
 
 #main loop
 root.mainloop()
